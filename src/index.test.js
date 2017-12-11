b3716b07758ba7d0d3ffc93a897ef31d6454b4a5
EGOIST <0x142857@gmail.com>
2017-12-11 14:38:17 +0800

    add basic test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,16 @@
 import { mount } from 'vue-test-utils'
 import SimplePortal from './'
 
+const $target = document.createElement('div')
+$target.id = 'target'
+document.body.appendChild($target)
+
 test('it works', () => {
-  const wrapper = mount(SimplePortal)
-  expect(wrapper.isVueInstance()).toBe(true)
+  const wrapper = mount({
+    template: '<simple-portal target="target">hi</simple-portal>',
+    components: {
+      SimplePortal
+    }
+  })
+  expect($target.outerHTML).toMatchSnapshot()
 })
